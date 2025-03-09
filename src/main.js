139d89fef226f24bc581f569bcdae6909c6833a6
lumexio <trotroyu@gmail.com>
2025-03-09 01:28:40 -0700

    feat: add electron-log dependency and implement logging in auto-updater functionality

--- src/main.js
+++ src/main.js
@@ -1,23 +1,22 @@
-import { app, autoUpdater, dialog, BrowserWindow } from 'electron';
+import { app, BrowserWindow } from 'electron';
 import { join } from 'node:path';
 import squirrelStartup from 'electron-squirrel-startup';
-import { updateElectronApp } from 'update-electron-app';
+import { updateElectronApp, UpdateSourceType } from 'update-electron-app';
+import { log } from 'electron-log'
+updateElectronApp({
+  repo: 'Lumexio/ps-electron',
+  updateInterval: '5 minutes',
+  updateType: UpdateSourceType.ElectronPublicUpdateService,
+  notifyUser: true,
+  logger: log,
+});
 
-import path from "path";
-import fs from "fs";
-import https from 'https';
-
-// Set global temporary directory for things like auto update downloads, creating it if it doesn't exist already.
-global.tempPath = path.join(app.getPath("temp"), "NTWRK");
-if (!fs.existsSync(global.tempPath)) fs.mkdirSync(global.tempPath);
 
 if (squirrelStartup) {
   app.quit();
 }
 
-app.setAppUserModelId("com.squirrel.StockMachine.StockMachine");
-autoUpdater.autoDownload = false;
-autoUpdater.autoInstallOnAppQuit = true;
+
 const createWindow = () => {
   const mainWindow = new BrowserWindow({
     width: 800,
@@ -45,26 +44,10 @@ const createWindow = () => {
   });
 };
 
-// Utility function to download files
-const downloadFile = (url, dest) => {
-  return new Promise((resolve, reject) => {
-    const file = fs.createWriteStream(dest);
-    https.get(url, (response) => {
-      response.pipe(file);
-      file.on('finish', () => {
-        file.close();
-        resolve();
-      });
-    }).on('error', (err) => {
-      fs.unlink(dest);
-      reject(err);
-    });
-  });
-};
+
 
 app.whenReady().then(() => {
 
-  updateElectronApp();
   createWindow();
 
   app.on('activate', () => {
@@ -72,81 +55,7 @@ app.whenReady().then(() => {
       createWindow();
     }
   });
-  const setupUpdater = async () => {
-    try {
-      const version = app.getVersion();
 
-      const baseUrl = `https://github.com/Lumexio/ps-electron/releases/download/${version}`;
-
-      if (process.platform === 'win32') {
-        // Windows specific setup
-        const releasesUrl = `${baseUrl}/RELEASES`;
-        const nupkgUrl = `${baseUrl}/STOCKMACHINE-${version}-full.nupkg`;
-
-        const releasesPath = path.join(global.tempPath, 'RELEASES');
-        const nupkgPath = path.join(global.tempPath, `STOCKMACHINE-${version}-full.nupkg`);
-
-        await Promise.all([
-          downloadFile(releasesUrl, releasesPath),
-          downloadFile(nupkgUrl, nupkgPath)
-        ]);
-        autoUpdater.setFeedURL(global.tempPath);
-        autoUpdater.checkForUpdates();
-
-        autoUpdater.on('update-available', () => {
-          dialog.showMessageBox({
-            type: 'info',
-            title: 'Update Available',
-            message: 'A new version of STOCKMACHINE is available!',
-            buttons: ['ok'],
-          });
-        });
-        autoUpdater.on('update-downloaded', (event, releaseNotes, releaseName) => {
-          const dialogOpts = {
-            type: 'info',
-            buttons: ['Restart', 'Later'],
-            title: 'Application Update',
-            message: process.platform === 'win32' ? releaseNotes : releaseName,
-            detail:
-              'A new version has been downloaded. Restart the application to apply the updates.'
-          }
-          dialog.showMessageBox(dialogOpts).then(() => {
-            autoUpdater.quitAndInstall()
-          })
-        })
-
-
-      } else if (process.platform === 'darwin') {
-        // macOS specific setup
-        const feedJson = {
-          url: `${baseUrl}/STOCKMACHINE-${version}-mac.zip`,
-          name: `${version}`,
-          notes: "New version available",
-          pub_date: new Date().toISOString()
-        };
-
-        const feedPath = path.join(global.tempPath, 'feed.json');
-        fs.writeFileSync(feedPath, JSON.stringify(feedJson));
-
-        autoUpdater.setFeedURL(`file://${feedPath}`);
-      }
-
-
-
-      console.log('Checking for updates from:', global.tempPath);
-      autoUpdater.on('update-not-available', () => {
-        dialog.showMessageBox({
-          type: 'info',
-          title: 'No Updates Available',
-          message: 'You are running the latest version of STOCKMACHINE!',
-          buttons: ['ok'],
-        });
-      });
-    } catch (error) {
-      console.error('Failed to setup auto-updater:', error);
-    }
-  };
-  setupUpdater();
 
 
 
